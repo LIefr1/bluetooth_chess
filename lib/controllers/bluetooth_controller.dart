ccc2d2593d890fefa0f35fa43af7c1600d941e06
LIefr1 <cstud129306@vyatsu.ru>
2023-11-04 12:40:55 +0300

    new

--- lib/controllers/bluetooth_controller.dart
+++ lib/controllers/bluetooth_controller.dart
@@ -1,11 +1,29 @@
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:get/get.dart';
 
-class BluetoothConroller extends GetxController {
-  Future scanDevices() async {
+class BluetoothController extends GetxController {
+  BluetoothDevice? connectedDevice;
+
+  Future<void> scanDevices() async {
     FlutterBluePlus.startScan(timeout: const Duration(seconds: 5));
-    // FlutterBluePlus.stopScan();
+  }
+
+  Future<void> connectDevice(BluetoothDevice device) async {
+    try {
+      await device.connect();
+      connectedDevice = device;
+      print('Connected to ${device.platformName}');
+      // Perform any actions after successful connection
+    } catch (e) {
+      print('Failed to connect to ${device.platformName}: $e');
+    }
+  }
+
+  void disconnectFromDevice() async {
+    await connectedDevice?.disconnect();
+    connectedDevice = null;
   }
 
   Stream<List<ScanResult>> get scanResults => FlutterBluePlus.scanResults;
+  BluetoothDevice? get selectedDevice => connectedDevice;
 }
--- lib/controllers/settings_page.dart
+++ lib/controllers/settings_page.dart
@@ -11,57 +11,70 @@ class Settings extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          GetBuilder<BluetoothConroller>(
-              init: BluetoothConroller(),
-              builder: (controller) {
-                return SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 100,
-                        width: double.infinity,
-                        color: Colors.blue,
-                        child: const Center(child: Text("Connect bluetooth")),
+          GetBuilder<BluetoothController>(
+            init: BluetoothController(),
+            builder: (controller) {
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: double.infinity,
+                      color: Colors.blue,
+                      child: const Center(child: Text("Connect bluetooth")),
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () => controller.scanDevices(),
+                        child: const Text("Scan"),
                       ),
-                      const SizedBox(height: 20),
-                      Center(
-                        child: ElevatedButton(
-                            onPressed: () => controller.scanDevices(),
-                            child: const Text("Scan")),
+                    ),
+                    const SizedBox(height: 20),
+                    StreamBuilder<List<ScanResult>>(
+                      stream: controller.scanResults,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              final data = snapshot.data![index];
+                              return Card(
+                                elevation: 2,
+                                child: ListTile(
+                                  title: Text(data.device.platformName),
+                                  subtitle: Text(data.device.remoteId.str),
+                                  trailing: Text(data.rssi.toString()),
+                                  onTap: () {
+                                    controller.connectDevice(data.device);
+                                  },
+                                ),
+                              );
+                            },
+                          );
+                        } else {
+                          return const Center(
+                            child: Card(
+                              elevation: 2,
+                              child: Text("No devices found"),
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                    if (controller.selectedDevice != null)
+                      ElevatedButton(
+                        onPressed: () {
+                          controller.disconnectFromDevice();
+                        },
+                        child: const Text("Disconnect"),
                       ),
-                      const SizedBox(height: 20),
-                      StreamBuilder<List<ScanResult>>(
-                          stream: controller.scanResults,
-                          builder: (context, snapshot) {
-                            print(snapshot.hasData);
-                            if (snapshot.hasData) {
-                              print(snapshot.data!.length);
-                              return ListView.builder(
-                                  shrinkWrap: true,
-                                  itemCount: snapshot.data!.length,
-                                  itemBuilder: (context, index) {
-                                    final data = snapshot.data![index];
-                                    return Card(
-                                      elevation: 2,
-                                      child: ListTile(
-                                        title: Text(data.device.platformName),
-                                        subtitle:
-                                            Text(data.device.remoteId.str),
-                                        trailing: Text(data.rssi.toString()),
-                                      ),
-                                    );
-                                  });
-                            } else {
-                              return const Center(
-                                  child: Card(
-                                      elevation: 2,
-                                      child: Text("No devices found")));
-                            }
-                          })
-                    ],
-                  ),
-                );
-              })
+                  ],
+                ),
+              );
+            },
+          ),
         ],
       ),
     );
